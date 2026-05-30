db9e17c777173558eb0147bd106b5e7478e861a1
Luiz Gustavo <luizgustavo974@hotmail.com>
2026-05-29 22:21:42 -0300

    chore: configuração inicial

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,8 +19,36 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      // Prettier integration
+      'prettier/prettier': 'warn',
+      ...eslintConfigPrettier.rules,
+
+      // Unused imports - auto-remove on save
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      // Import sorting - auto-sort on save
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
     },
   },
 ])
