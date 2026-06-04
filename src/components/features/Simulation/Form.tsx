c15e15423856f003294bbf5425bdc681e98968c0
Luiz Gustavo <luizgustavo974@hotmail.com>
2026-06-04 15:48:11 -0300

    feat: criar componente FormStep

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,9 +1,22 @@
+import { PiggyBank } from 'lucide-react'
+
+import { FormStep } from './FormStep'
 import { StepProgress } from './Progress'
 
 export const SimulationForm = () => {
   return (
     <>
       <StepProgress currentStep={3} totalSteps={6} />
+      <FormStep
+        icon={PiggyBank}
+        title="Renda mensal bruta"
+        question="Quanto é depositado na sua conta todo mês (somando todas as fontes)?"
+        inputProps={{
+          type: 'text',
+          placeholder: 'ex: 5.000,00',
+          prefix: 'R$',
+        }}
+      />
     </>
   )
 }
